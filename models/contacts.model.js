848b3a8a38f2cef2d71ffa807f09abe0ce6d3a46
Stanislav Taran <StanislavTaran@users.noreply.github.com>
2020-07-12 12:22:47 +0300

    update contact model operations

--- models/contacts.model.js
+++ models/contacts.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { ObjectId } = require("mongoose").Types;
 
 const contactSchema = mongoose.Schema(
   {
@@ -11,6 +12,7 @@ const contactSchema = mongoose.Schema(
     email: {
       type: String,
       required: true,
+      unique: true,
       trim: true,
     },
     phone: {
@@ -26,85 +28,32 @@ class Contact {
   constructor() {
     this.contact = mongoose.model("Contact", contactSchema);
   }
+
+  getListContacts = async (query = {}) => {
+    return await this.contact.find(query);
+  };
+
+  getContactById = async (contactId) => {
+    return ObjectId.isValid(contactId)
+      ? await this.contact.findById(contactId)
+      : null;
+  };
+
+  removeContact = async (contactId) => {
+    return ObjectId.isValid(contactId)
+      ? await this.contact.findByIdAndDelete(contactId)
+      : null;
+  };
+
+  addContact = async (contact) => {
+    return await this.contact.create(contact);
+  };
+
+  updateContact = async (contactId, newData) => {
+    return await this.contact.findByIdAndUpdate(contactId, newData, {
+      new: true,
+    });
+  };
 }
 
-const getListContacts = async (query = {}) => {
-  await this.contact
-    .find(query)
-    .then((res) => {
-      return JSON.parse(res);
-    })
-    .catch((error) => {
-      throw error;
-    });
-
-  return contacts;
-};
-
-const getContactById = async (contactId) => {
-  const allContacts = await getListContacts();
-  const foundContact = allContacts.find((contact) => contact.id === +contactId);
-
-  return foundContact;
-};
-
-const removeContact = async (contactId) => {
-  const allContacts = await getListContacts();
-  const foundContact = await allContacts.find(
-    (contact) => contact.id === +contactId
-  );
-
-  if (foundContact) {
-    const newListContacts = await allContacts.filter(
-      (contact) => contact.id !== contactId
-    );
-    await fs
-      .writeFile(contactsPath, JSON.stringify(newListContacts))
-      .then(() => {
-        console.log(`\x1B[34m File with ID - ${contactId} - removed! \x1b[0m`);
-      })
-      .catch((error) => {
-        throw error;
-      });
-  } else console.log(`\x1B[31m Contact not found! \x1b[0m`);
-  return foundContact;
-};
-
-const addContact = async (name, email, phone) => {
-  const allContacts = await getListContacts();
-  const newId = [...allContacts].pop().id + 1;
-  const createdContact = new Contact({ name, email, phone }, newId);
-  allContacts.push(createdContact);
-  await fs
-    .writeFile(contactsPath, JSON.stringify(allContacts))
-    .then(() => {
-      console.log(`\x1B[32m Added contact ${name}! \x1b[0m`, createdContact);
-    })
-    .catch((error) => {
-      throw error;
-    });
-  return createdContact;
-};
-
-const updateContact = async (id, value) => {
-  const contacts = (await getListContacts()) || [];
-  const contact = (await getContactById(id)) || null;
-
-  if (contact) {
-    const newContact = { ...contact, ...value };
-    const newContacts = contacts.map((contact) =>
-      contact.id !== newContact.id ? contact : newContact
-    );
-
-    await fs.writeFile(contactsPath, JSON.stringify(newContacts));
-    return newContact;
-  }
-};
-
-module.exports = {
-  getListContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-};
+module.exports = new Contact();
